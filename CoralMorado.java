249b53162db7adf8eaaaf1f39128a4955fd00abe
Fernanda Esquivel <esq21542@uvg.edu.gt>
2021-05-19 19:22:05 -0600

    Objetos
    Se editó el pez.
    Se agregaron los tres corales.

--- CoralMorado.java
+++ CoralMorado.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Estrella here.
+ * Write a description of class CoralMorado here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Estrella extends Actor
+public class CoralMorado extends Actor
 {
     /**
-     * Act - do whatever the Estrella wants to do. This method is called whenever
+     * Act - do whatever the CoralMorado wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,32 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(416, 608, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Pez pez = new Pez();
+        addObject(pez,181,394);
+        CoralAmarillo coralAmarillo = new CoralAmarillo();
+        addObject(coralAmarillo,57,214);
+        CoralMorado coralMorado = new CoralMorado();
+        addObject(coralMorado,352,217);
+        coralAmarillo.setLocation(267,201);
+        CoralRosa coralRosa = new CoralRosa();
+        addObject(coralRosa,169,244);
+        coralRosa.setLocation(61,211);
+        coralAmarillo.setLocation(317,352);
+        coralMorado.setLocation(212,213);
+        coralAmarillo.setLocation(362,191);
+        CoralAmarillo coralAmarillo2 = new CoralAmarillo();
+        addObject(coralAmarillo2,164,62);
+        CoralRosa coralRosa2 = new CoralRosa();
+        addObject(coralRosa2,336,378);
     }
 }
