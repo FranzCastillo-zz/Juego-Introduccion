4d4b01332c72f1981e597121ce62d65bb6fbdc6d
Francisco Castillo <cas21562@uvg.edu.gt>
2021-05-19 19:31:05 -0600

    Agrega Objeto Invisible
    El objeto invisible para la barrera de arrecife ha sido agregado.

--- MyWorld.java
+++ MyWorld.java
@@ -43,5 +43,35 @@ public class MyWorld extends World
         addObject(coralAmarillo2,164,62);
         CoralRosa coralRosa2 = new CoralRosa();
         addObject(coralRosa2,336,378);
+        coralRosa.setLocation(61,192);
+        coralMorado.setLocation(268,200);
+        coralRosa.setLocation(50,177);
+        coralAmarillo2.setLocation(209,202);
+        coralRosa.setLocation(43,195);
+        coralAmarillo.setLocation(344,399);
+        coralMorado.setLocation(354,187);
+        coralAmarillo2.setLocation(258,206);
+        coralRosa.setLocation(64,208);
+        pez.setLocation(222,540);
+        pez.setLocation(196,555);
+        coralRosa.setLocation(60,216);
+        coralAmarillo.setLocation(311,374);
+        coralAmarillo2.setLocation(259,207);
+        pez.setLocation(218,555);
+        coralAmarillo.setLocation(330,377);
+        coralAmarillo.setLocation(81,390);
+        coralRosa2.setLocation(81,390);
+        pez.setLocation(200,548);
+        coralRosa2.setLocation(108,380);
+        coralRosa2.setLocation(84,396);
+        coralRosa2.setLocation(99,387);
+        removeObject(coralRosa2);
+        removeObject(coralAmarillo);
+        coralAmarillo2.setLocation(264,270);
+        coralMorado.setLocation(336,248);
+        coralRosa.setLocation(49,255);
+        coralMorado.setLocation(336,245);
+        coralAmarillo2.setLocation(249,251);
+        coralMorado.setLocation(354,255);
     }
 }
