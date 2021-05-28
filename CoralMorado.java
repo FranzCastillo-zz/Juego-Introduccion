d57a7f5389047465c6eacb5da2a45f946c7edb89
TheKiesling <kie21581@uvg.edu.gt>
2021-05-28 15:24:44 -0600

    Terminar corales
    Documentación interna y optimización

--- CoralMorado.java
+++ CoralMorado.java
@@ -1,32 +1,27 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class ferrari here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * José Kiesling 
+ * CoralAmarillo
+ * 20 de mayo
  */
 public class CoralMorado extends Actor
 {
-    private int velocidad;
+    private int velocidad; //Variable de velocidad
     
     public CoralMorado(int v){
         velocidad = v;
     }
-    /**
-     * Act - do whatever the ferrari wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    //Acción del coral
     public void act() 
     {
-        // Add your action code here.
-        setLocation(getX(), getY() + velocidad);
-        if( getY() >= getWorld().getHeight() - 1 ){
-            MyWorld juego = (MyWorld) getWorld();
-            juego.removeObject(this);
-            juego.aumentar_puntuacion(10);
-            juego.disminuir_num_corales();
-            juego.aumentar_num_adelantamientos();
+        setLocation(getX(), getY() + velocidad); //Obtener posición del coral
+        if( getY() >= getWorld().getHeight() - 1 ){ // Condición que evalúa si ha llegado al fondo de la panatalla
+            MyWorld juego = (MyWorld) getWorld(); //Llamar 
+            juego.removeObject(this); //Eliminar el objeto
+            juego.aumentar_puntuacion(10); //Aumentar la puntuación 
+            juego.disminuir_num_corales(); //Disminuir el número de corales
+            juego.aumentar_num_adelantamientos(); //Aumentar la velocidad
         }
     }    
 }
